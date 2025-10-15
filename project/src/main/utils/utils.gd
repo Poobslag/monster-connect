0ccba428779f45406c70824d041b561e2fb35e54
Poobslag <aaron.pieper@gmail.com>
2025-10-15 14:10:55 -0400

    Added utils: intersection, subtract

--- project/src/main/utils/utils.gd
+++ project/src/main/utils/utils.gd
@@ -12,6 +12,11 @@ static func kill_tween(tween: Tween) -> Tween:
 	return null
 
 
+## Associates [param key] with [param value] if the key does not already exist.
+static func put_if_absent(dict: Dictionary[Variant, Variant], key: Variant, value: Variant) -> void:
+	dict[key] = dict.get(key, value)
+
+
 ## Creates/recreates a tween, invalidating it if it is already active.[br]
 ## [br]
 ## Tweens should be created and discarded, but tweening the same property with multiple tweens causes unpredictable
@@ -21,3 +26,36 @@ static func kill_tween(tween: Tween) -> Tween:
 static func recreate_tween(node: Node, tween: Tween) -> Tween:
 	kill_tween(tween)
 	return node.create_tween()
+
+
+## Returns a new array containing the intersection of the given arrays.
+static func intersection(a: Array[Variant], b: Array[Variant]) -> Array[Variant]:
+	var result: Array[Variant] = []
+	var bag: Dictionary[Variant, int] = {}
+	for item: Variant in b:
+		put_if_absent(bag, item, 0)
+		bag[item] += 1
+	for item: Variant in a:
+		if bag.has(item):
+			bag[item] -= 1
+			if bag[item] == 0:
+				bag.erase(item)
+			result.append(item)
+	return result
+
+
+## Returns a new array containing [param a] - [param b].
+static func subtract(a: Array[Variant], b: Array[Variant]) -> Array[Variant]:
+	var result: Array[Variant] = []
+	var bag: Dictionary[Variant, int] = {}
+	for item: Variant in b:
+		put_if_absent(bag, item, 0)
+		bag[item] += 1
+	for item: Variant in a:
+		if bag.has(item):
+			bag[item] -= 1
+			if bag[item] == 0:
+				bag.erase(item)
+		else:
+			result.append(item)
+	return result
