46c066a856e69c5f1eeae81bc57e6d25b8702496
Poobslag <aaron.pieper@gmail.com>
2025-10-29 10:54:42 -0400

    Tests

--- project/src/test/nurikabe/test_grid_connectivity_map.gd
+++ project/src/test/nurikabe/test_grid_connectivity_map.gd
@@ -1,7 +1,6 @@
 extends GutTest
 
 var gcm := GridConnectivityMap.new()
-var grid: Array[String] = []
 
 func before_each() -> void:
 	gcm.clear()
@@ -28,35 +27,93 @@ func test_single_inactive_cell() -> void:
 
 
 func test_two_cells_adjacent() -> void:
-	gcm.set_active(Vector2i(0, 0), true)
-	gcm.set_active(Vector2i(1, 0), true)
+	load_grid([
+		"12",
+	])
 	assert_groups([[Vector2i(0, 0), Vector2i(1, 0)]])
 
 
 func test_two_cells_separate() -> void:
-	gcm.set_active(Vector2i(0, 0), true)
-	gcm.set_active(Vector2i(2, 0), true)
+	load_grid([
+		"1 2",
+	])
 	assert_groups([[Vector2i(0, 0)], [Vector2i(2, 0)]])
 
 
 func test_shrink_group() -> void:
-	gcm.set_active(Vector2i(0, 0), true)
-	gcm.set_active(Vector2i(1, 0), true)
+	load_grid([
+		"12",
+	])
 	gcm.set_active(Vector2i(0, 0), false)
 	assert_groups([[Vector2i(1, 0)]])
 
 
 func test_remove_group() -> void:
-	pass
+	gcm.set_active(Vector2i(0, 0), true)
+	gcm.set_active(Vector2i(0, 0), false)
+	assert_groups([])
 
 
-# remove group
-# merge two groups
-# merge three groups
-# merge four groups
-# split two groups
-# split three groups
-# split four groups
+func test_merge_groups_two() -> void:
+	load_grid([
+		"13",
+		" 2",
+	])
+	assert_groups([[Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1)]])
+
+
+func test_merge_groups_three() -> void:
+	load_grid([
+		"143",
+		" 2",
+	])
+	assert_groups([[Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 0)]])
+
+
+func test_merge_groups_four() -> void:
+	load_grid([
+		" 1 ",
+		"254",
+		" 3",
+	])
+	assert_groups([[Vector2i(0, 1), Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 1)]])
+
+
+func test_split_groups_two() -> void:
+	load_grid([
+		"123",
+	])
+	gcm.set_active(Vector2i(1, 0), false)
+	assert_groups([[Vector2i(0, 0)], [Vector2i(2, 0)]])
+
+
+func test_split_groups_three() -> void:
+	load_grid([
+		"123",
+		" 4 ",
+	])
+	gcm.set_active(Vector2i(1, 0), false)
+	assert_groups([[Vector2i(0, 0)], [Vector2i(1, 1)], [Vector2i(2, 0)]])
+
+
+func test_split_groups_four() -> void:
+	load_grid([
+		" 1 ",
+		"234",
+		" 5 ",
+	])
+	gcm.set_active(Vector2i(1, 1), false)
+	assert_groups([[Vector2i(0, 1)], [Vector2i(1, 0)], [Vector2i(1, 2)], [Vector2i(2, 1)]])
+
+
+func test_split_groups_half() -> void:
+	load_grid([
+		"1 4",
+		"235",
+		"  6",
+	])
+	gcm.set_active(Vector2i(1, 1), false)
+	assert_groups([[Vector2i(0, 0), Vector2i(0, 1)], [Vector2i(2, 0), Vector2i(2, 1), Vector2i(2, 2)]])
 
 
 func assert_groups(expected: Array[Array]) -> void:
@@ -64,6 +121,22 @@ func assert_groups(expected: Array[Array]) -> void:
 	var actual_sorted: Array[Array] = sort_groups(actual)
 	var expected_sorted: Array[Array] = sort_groups(expected)
 	assert_eq(actual_sorted, expected_sorted)
+
+
+func load_grid(rows: Array[String]) -> void:
+	var active_cells: Array[Dictionary] = []
+	for y in rows.size():
+		var row := rows[y]
+		for x in row.length():
+			var ch := row[x]
+			if ch == " ":
+				continue
+			active_cells.append({"label": int(ch), "pos": Vector2i(x, y)} as Dictionary[String, Variant])
+	active_cells.sort_custom(
+		func(a: Dictionary[String, Variant], b: Dictionary[String, Variant]) -> bool:
+			return a.label < b.label)
+	for entry: Dictionary[String, Variant] in active_cells:
+		gcm.set_active(entry.pos, true)
 
 
 func sort_groups(groups: Array[Array]) -> Array[Array]:
