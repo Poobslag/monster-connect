9e38e5b2f209af4de496d91aa0cbf465eef39840
Poobslag <aaron.pieper@gmail.com>
2025-11-06 08:28:18 -0500

    Increased idle_steps threshold for fast solver.
    100 is, for some reason, not enough for puzzle 41. Will investigate.

--- project/src/demo/nurikabe/fast/demo_fast_solver.gd
+++ project/src/demo/nurikabe/fast/demo_fast_solver.gd
@@ -80,7 +80,7 @@ func performance_test() -> void:
 	var start_time: float = Time.get_ticks_usec()
 	
 	var idle_steps: int = 0
-	while idle_steps < 100 and not solver.board.is_filled():
+	while idle_steps < 200 and not solver.board.is_filled():
 		var old_filled_cell_count: int = solver.board.get_filled_cell_count()
 		
 		solver.step()
