b3f08d9298a456da2c66712d791ea8071802ffc7
Poobslag <aaron.pieper@gmail.com>
2025-12-12 12:18:21 -0600

    Avoid iterating over SolverBoard.cells.
    Boards have a lot of cells; it's better to iterate over empty cells or a
    subset of cells.

--- project/src/test/nurikabe/solver/test_global_reachability_map.gd
+++ project/src/test/nurikabe/solver/test_global_reachability_map.gd
@@ -10,10 +10,10 @@ func test_get_clue_reachability() -> void:
 	]
 	var grm: GlobalReachabilityMap = init_global_reachability_map()
 	assert_eq(grm.get_clue_reachability(Vector2(0, 0)), GlobalReachabilityMap.ClueReachability.UNREACHABLE)
-	assert_eq(grm.get_clue_reachability(Vector2(0, 2)), GlobalReachabilityMap.ClueReachability.IMPOSSIBLE)
+	assert_eq(grm.get_clue_reachability(Vector2(0, 2)), GlobalReachabilityMap.ClueReachability.UNKNOWN)
 	assert_eq(grm.get_clue_reachability(Vector2(0, 1)), GlobalReachabilityMap.ClueReachability.REACHABLE)
 	assert_eq(grm.get_clue_reachability(Vector2(1, 2)), GlobalReachabilityMap.ClueReachability.CONFLICT)
-	assert_eq(grm.get_clue_reachability(Vector2(2, 0)), GlobalReachabilityMap.ClueReachability.IMPOSSIBLE)
+	assert_eq(grm.get_clue_reachability(Vector2(2, 0)), GlobalReachabilityMap.ClueReachability.UNKNOWN)
 	assert_eq(grm.get_clue_reachability(Vector2(3, 0)), GlobalReachabilityMap.ClueReachability.IMPOSSIBLE)
 
 
