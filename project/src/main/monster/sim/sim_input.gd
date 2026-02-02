ba0d60ac251756a5565454d9c48280f099ac5b65
Poobslag <aaron.pieper@gmail.com>
2026-02-02 14:26:01 -0500

    Sims should no longer unclick deductions.
    Before, sims would sometimes "fight with the player" when they tried to
    make the same deduction. They'd even "fight with themselves" sometimes
    if they came about the same deduction multiple ways.
    
    They now track "cursor commands by cell" to understand, "I'm already
    going to click that cell," and "Whoa, the cell I was going to click
    changed!"

--- project/src/main/monster/sim/sim_input.gd
+++ project/src/main/monster/sim/sim_input.gd
@@ -36,8 +36,33 @@ func move_to(target: Vector2) -> void:
 		dir = Vector2.ZERO
 
 
-func queue_cursor_command(action: CursorAction, pos: Vector2, delay: float = 0.0) -> void:
-	_cursor_commands.append(CursorCommand.new(action, pos, delay))
+func queue_cursor_command(action: CursorAction, pos: Vector2, delay: float = 0.0) -> CursorCommand:
+	var command: CursorCommand = CursorCommand.new(action, pos, delay)
+	_cursor_commands.append(command)
+	return command
+
+
+func has_cursor_command(command: CursorCommand) -> bool:
+	return command in _cursor_commands
+
+
+func dequeue_cursor_command(command: CursorCommand) -> void:
+	var command_index: int = _cursor_commands.find(command)
+	if command_index == -1:
+		return
+	
+	_cursor_commands.remove_at(command_index)
+	if command_index == 0:
+		if %PuzzleHandler.lmb_pressed:
+			var event: InputEventMouseButton = InputEventMouseButton.new()
+			event.button_index = MOUSE_BUTTON_LEFT
+			event.pressed = false
+			%PuzzleHandler.handle(event)
+		if %PuzzleHandler.rmb_pressed:
+			var event: InputEventMouseButton = InputEventMouseButton.new()
+			event.button_index = MOUSE_BUTTON_RIGHT
+			event.pressed = false
+			%PuzzleHandler.handle(event)
 
 
 func _process_cursor_command(delta: float) -> void:
