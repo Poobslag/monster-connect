2b3881ed36a3d9dd29812bec59458f39aac5e0b6
Poobslag <aaron.pieper@gmail.com>
2026-02-04 11:32:15 -0500

    Recategorize techniques: break-in, easy, standard, advanced
    The old categories of "starting, basic, advanced" weren't quite nuanced
    enough. There is a big difference between seeing "this completed
    3-island should be surrounded by a wall" and "this island chain
    needs a buffer cell to avoid an eventual split wall"

--- project/src/main/nurikabe/reason_code.gd
+++ project/src/main/nurikabe/reason_code.gd
@@ -2,34 +2,36 @@ class_name ReasonCode
 
 const UNKNOWN_REASON: Deduction.Reason = Deduction.Reason.UNKNOWN
 
-## starting reasons
+## break-in techniques
 const ISLAND_OF_ONE: Deduction.Reason = Deduction.Reason.ISLAND_OF_ONE
 const ADJACENT_CLUES: Deduction.Reason = Deduction.Reason.ADJACENT_CLUES
 
-## basic reasons
+## easy techniques
+const ISLAND_BUBBLE: Deduction.Reason = Deduction.Reason.ISLAND_BUBBLE
+const ISLAND_DIVIDER: Deduction.Reason = Deduction.Reason.ISLAND_DIVIDER
+const ISLAND_EXPANSION: Deduction.Reason = Deduction.Reason.ISLAND_EXPANSION
+const ISLAND_MOAT: Deduction.Reason = Deduction.Reason.ISLAND_MOAT
+const POOL_TRIPLET: Deduction.Reason = Deduction.Reason.POOL_TRIPLET
+const WALL_BUBBLE: Deduction.Reason = Deduction.Reason.WALL_BUBBLE
+const WALL_EXPANSION: Deduction.Reason = Deduction.Reason.WALL_EXPANSION
+
+## standard techniques
 const CORNER_BUFFER: Deduction.Reason = Deduction.Reason.CORNER_BUFFER
 const CORNER_ISLAND: Deduction.Reason = Deduction.Reason.CORNER_ISLAND
-const ISLAND_BUBBLE: Deduction.Reason = Deduction.Reason.ISLAND_BUBBLE
 const ISLAND_BUFFER: Deduction.Reason = Deduction.Reason.ISLAND_BUFFER
 const ISLAND_CHAIN: Deduction.Reason = Deduction.Reason.ISLAND_CHAIN
 const ISLAND_CHAIN_BUFFER: Deduction.Reason = Deduction.Reason.ISLAND_CHAIN_BUFFER
 const ISLAND_CHOKEPOINT: Deduction.Reason = Deduction.Reason.ISLAND_CHOKEPOINT
 const ISLAND_CONNECTOR: Deduction.Reason = Deduction.Reason.ISLAND_CONNECTOR
-const ISLAND_DIVIDER: Deduction.Reason = Deduction.Reason.ISLAND_DIVIDER
-const ISLAND_EXPANSION: Deduction.Reason = Deduction.Reason.ISLAND_EXPANSION
-const ISLAND_MOAT: Deduction.Reason = Deduction.Reason.ISLAND_MOAT
 const ISLAND_SNUG: Deduction.Reason = Deduction.Reason.ISLAND_SNUG
 const POOL_CHOKEPOINT: Deduction.Reason = Deduction.Reason.POOL_CHOKEPOINT
-const POOL_TRIPLET: Deduction.Reason = Deduction.Reason.POOL_TRIPLET
 const UNCLUED_LIFELINE: Deduction.Reason = Deduction.Reason.UNCLUED_LIFELINE
 const UNCLUED_LIFELINE_BUFFER: Deduction.Reason = Deduction.Reason.UNCLUED_LIFELINE_BUFFER
 const UNREACHABLE_CELL: Deduction.Reason = Deduction.Reason.UNREACHABLE_CELL
-const WALL_BUBBLE: Deduction.Reason = Deduction.Reason.WALL_BUBBLE
 const WALL_CONNECTOR: Deduction.Reason = Deduction.Reason.WALL_CONNECTOR
-const WALL_EXPANSION: Deduction.Reason = Deduction.Reason.WALL_EXPANSION
 const WALL_WEAVER: Deduction.Reason = Deduction.Reason.WALL_WEAVER
 
-## advanced reasons
+## advanced techniques
 const ASSUMPTION: Deduction.Reason = Deduction.Reason.ASSUMPTION
 const BORDER_HUG: Deduction.Reason = Deduction.Reason.BORDER_HUG
 const ISLAND_BATTLEGROUND: Deduction.Reason = Deduction.Reason.ISLAND_BATTLEGROUND
@@ -40,11 +42,18 @@ const WALL_STRANGLE: Deduction.Reason = Deduction.Reason.WALL_STRANGLE
 const CODES: Dictionary[String, Deduction.Reason] = {
 	"-": UNKNOWN_REASON,
 	
-	# starting reasons
+	# break-in techniques
 	"i1": ISLAND_OF_ONE,
 	"ac": ADJACENT_CLUES,
 	
-	# basic reasons
+	# easy techniques
+	"ix": ISLAND_EXPANSION,
+	"im": ISLAND_MOAT,
+	"p3": POOL_TRIPLET,
+	"wx": WALL_EXPANSION,
+	"id": ISLAND_DIVIDER,
+	
+	# standard techniques
 	"cb": CORNER_BUFFER,
 	"ci": CORNER_ISLAND,
 	"ib": ISLAND_BUBBLE,
@@ -53,20 +62,16 @@ const CODES: Dictionary[String, Deduction.Reason] = {
 	"iC": ISLAND_CHAIN_BUFFER,
 	"ik": ISLAND_CHOKEPOINT,
 	"ie": ISLAND_CONNECTOR,
-	"id": ISLAND_DIVIDER,
-	"ix": ISLAND_EXPANSION,
-	"im": ISLAND_MOAT,
 	"is": ISLAND_SNUG,
 	"pk": POOL_CHOKEPOINT,
-	"p3": POOL_TRIPLET,
 	"ul": UNCLUED_LIFELINE,
 	"uL": UNCLUED_LIFELINE_BUFFER,
 	"ur": UNREACHABLE_CELL,
 	"wb": WALL_BUBBLE,
 	"we": WALL_CONNECTOR,
-	"wx": WALL_EXPANSION,
 	"wv": WALL_WEAVER,
 	
+	# advanced techniques
 	"??": ASSUMPTION,
 	"Bh": BORDER_HUG,
 	"Bg": ISLAND_BATTLEGROUND,
