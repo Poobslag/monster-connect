80ddcc33416e39b572eac48437ca56fa16077ecf
Poobslag <aaron.pieper@gmail.com>
2026-02-11 10:55:11 -0500

    comment

--- project/src/main/monster/sim/sim_behavior.gd
+++ project/src/main/monster/sim/sim_behavior.gd
@@ -25,6 +25,10 @@ func get_stat(stat: String) -> float:
 	return inverse_lerp(0, 10, raw)
 
 
+## Extrapolates a stat along a range with an optional midpoint.[br]
+## [br]
+## This method accepts a stat, a min/max and an optional midpoint, and returns a biased interpolation following an
+## exponential ease. This makes it easier to convert stat values like "0.5" to useful numbers like "300 milliseconds".
 func lerp_stat(stat: String, from: float, to: float, avg: float = (from + to) / 2.0) -> float:
 	var scaled_avg: float = inverse_lerp(from, to, avg)
 	var weight: float = pow(get_stat(stat), log(scaled_avg) / log(0.5))
