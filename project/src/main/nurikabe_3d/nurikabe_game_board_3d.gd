8c4601b7349dbcc90f976afb8a1241c12a492e09
Poobslag <aaron.pieper@gmail.com>
2026-03-02 13:40:59 -0500

    Added empty 3d scene. Added game board, ground layer.
    Game board has about 4 more layers to go before it can render an actual
    puzzle, with walls, clues, islands and errors.

---
+++ project/src/main/nurikabe_3d/nurikabe_game_board_3d.gd
@@ -0,0 +1,36 @@
+@tool
+extends Node3D
+
+const CELL_INVALID: int = NurikabeUtils.CELL_INVALID
+const CELL_ISLAND: int = NurikabeUtils.CELL_ISLAND
+const CELL_WALL: int = NurikabeUtils.CELL_WALL
+const CELL_EMPTY: int = NurikabeUtils.CELL_EMPTY
+
+@export_multiline var grid_string: String
+
+@export_tool_button("Import Grid String") var import_grid_action: Callable = import_grid
+
+@export var tile_size: Vector2 = Vector2(1, 1)
+
+
+func _ready() -> void:
+	%GroundLayer.tile_size = tile_size
+	
+	import_grid()
+
+
+func import_grid() -> void:
+	%GroundLayer.clear()
+	
+	var cells: Dictionary[Vector2i, int] = NurikabeUtils.cells_from_grid_string(grid_string)
+	for cell: Vector2i in cells:
+		set_cell(cell, cells[cell])
+
+
+func set_cell(cell_pos: Vector2i, value: int, _player_id: int = -1) -> void:
+	# update cell value
+	_set_cell_internal(cell_pos, value)
+
+
+func _set_cell_internal(cell_pos: Vector2i, value: int) -> void:
+	%GroundLayer.set_cell(cell_pos, value)
