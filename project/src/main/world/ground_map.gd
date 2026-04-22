ad8732768169288fce34dad82ed3e263289461b7
Poobslag <aaron.pieper@gmail.com>
2026-04-22 11:16:08 -0400

    Removed unused 'steppable' logic. Fixed warning.

--- project/src/main/world/ground_map.gd
+++ project/src/main/world/ground_map.gd
@@ -2,7 +2,7 @@
 extends GridMap
 
 ## Template for running arbitrary code in the editor
-@export_tool_button("Editor Action") var editor_action: Callable = func():
+@export_tool_button("Editor Action") var editor_action: Callable = func() -> void:
 	#for x in range(-75, 75):
 	#	for z in range(-75, 75):
 	#		set_cell_item(Vector3(x, -1, z), 0)
